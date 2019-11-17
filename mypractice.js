33b88b5d3006dcfd219d1bff1d77f7828bad6543
chani24 <52843314+chani24@users.noreply.github.com>
2019-11-17 08:15:56 +0100

    Initial commit

---
+++ mypractice.js
@@ -0,0 +1,5 @@
+console.log(24);
+var first = document.getElementById('tribute-info');
+first.innerText = "new world Afrobeat";
+first.innerHTML = "<sup>new world Afrobeat</sup>";
+first.onclick = function (params) {first.style.cssText = "color: white"};
